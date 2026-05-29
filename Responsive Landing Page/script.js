7fc6cda59fcb838a749dcb8718148780c9758afc
Ashritha Gorre <ashrithag821@gmail.com>
2026-05-29 12:02:57 +0530

    folder deleted

--- Responsive Landing Page/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('scroll', function() {
-    const navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) { // Adjust the scrollY value as needed
-        navbar.classList.add('nav-scrolled');
-    } else {
-        navbar.classList.remove('nav-scrolled');
-    }
-});
